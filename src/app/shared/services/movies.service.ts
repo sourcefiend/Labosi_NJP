262876ec7a8391875b1dd11f7fef2656b9413efb
Robert Smolčić <rsmolcic@omega-software.hr>
2021-11-16 22:55:48 +0100

    fixevi

--- src/app/shared/services/movies.service.ts
+++ src/app/shared/services/movies.service.ts
@@ -1,17 +1,47 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-
+import { EventEmitter, Injectable } from '@angular/core';
 import { Movie } from '../models/movie';
 
 @Injectable()
 export class MovieService {
 
-    constructor(private http: HttpClient) { }
+    private movies!: Movie[];
+    
+    public movieAdded = new EventEmitter();
 
-    getMovies() {
+    constructor(private http: HttpClient) {
+        this.fetchMovies().then((fetchedMovies: any) => {
+            this.movies = fetchedMovies.map((movie: any) => {
+                // HACK: zašto se ovo samo ne mapira ako pridodjeljuje modelu (type coercion??)
+               movie.year = parseInt(movie.year, 10);
+               movie.rating = parseFloat(movie.rating);
+               return movie;
+            });
+        })
+    }
+
+    fetchMovies() {
         return this.http.get('https://bridge.hr/ipw/imdb.json')
             .toPromise()
             .then(movies => <Movie[]> movies)
             .then(movies => { return movies; })
+    }
+
+    addMovie(movie: Movie): void {
+        const maxIndex = Math.max(...this.movies.map(movie => movie.index.valueOf()));
+        movie.index = maxIndex + 1;
+        this.movies.push(movie);
+        this.movieAdded.emit();
+    }
+
+    deleteMovie(movie: Movie): void {
+        const index = this.movies.indexOf(movie);
+        if (index > -1) {
+            this.movies.splice(index, 1);
+        }
+    }
+
+    getMovies(): Movie[] {
+        return this.movies;
     }
 }
